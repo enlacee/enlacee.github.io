1243f82be55ee9f22967f7be6fd3da59fcedad74
Anibal Copitan <github@pprios.com>
2025-03-31 23:00:01 -0500

    minor: generate html for production

---
+++ docs/nuxt/static/1743479949/linktree/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1743479949",layout:"linktree",error:a,serverRendered:true,routePath:"\u002Flinktree",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"ee3a7834"}}}}(null));
---
+++ docs/nuxt/static/1743479949/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1743479949",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"ee3a7834"}}}}(null,"\u002F"));
