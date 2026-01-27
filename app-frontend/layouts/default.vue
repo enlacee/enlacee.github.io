a608eea98c73abf7d9b45db797d12d61b8e9fe0b
Anibal <git@anibalcopitan.com>
2026-01-26 23:17:33 -0500

    Remove the last project Portfolio & blog

--- app-frontend/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <Nuxt />
-</template>
-<style>
-/*
-  @import '@/assets/css/bootstrap.min.css';
-  @import '@/assets/css/freelancer.css';
-*/
-</style>
--- app-frontend/middleware/redirect-to-https.js
+++
@@ -1,5 +0,0 @@
-export default function ({route}) {
-
-  // console.log('redirect but variables global not exit HERE!', process.env.NODE_ENV);
-  // console.log(route);
-}
